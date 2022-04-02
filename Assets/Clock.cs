17518391219e759dfcb1bd586214c18732a71c72
zombonline <megazombie130@gmail.com>
2022-04-02 22:25:42 +0100

    My clock idea!

---
+++ Assets/Clock.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] float maxTime, unlockedTime;
+    float timeRemaining;
+    [SerializeField] Image unlockedTimeImage, clockArmImage;
+    private void Awake()
+    {
+        timeRemaining = unlockedTime;
+    }
+    private void Update()
+    {
+        unlockedTimeImage.fillAmount = unlockedTime / maxTime;
+
+        clockArmImage.rectTransform.eulerAngles = new Vector3(0, 0, -((timeRemaining / maxTime) * 360));
+
+        timeRemaining -= Time.deltaTime;
+    }
+
+
+    public void IncreaseUnlockedTime(float increaseValue)
+    {
+        unlockedTime += increaseValue;
+        if(unlockedTime > maxTime)
+        {
+            unlockedTime = maxTime;
+        }
+    }
+}
